d8730311e5052e522fb06efa703f6786a2720296
badrish mishra <badrish.kumar.mishra@gmail.com>
2020-10-15 00:06:38 +0100

    chages in files

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -5,3 +5,9 @@ echo '(written to ".pidfile"), all of which were conducted when either'
 echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
 set -x
 kill $(cat .pidfile)
+
+
+echo 'The following command terminates the "npm start" process using its PID'
+echo '(written to ".pidfile"), all of which were conducted when either'
+echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
+set -x
