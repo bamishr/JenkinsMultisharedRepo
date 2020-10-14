7b3a5d3b2a4148131ead53533d3e971f81842380
badrish mishra <badrish.kumar.mishra@gmail.com>
2020-10-15 00:08:57 +0100

    chages in files

--- jenkins/scripts/deliver-for-development.sh
+++ jenkins/scripts/deliver-for-development.sh
@@ -4,6 +4,9 @@ echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
 echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+echo $! > .pidfile
+set +x
+
 echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
